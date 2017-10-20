5cb63aea1fb60119f1b10022f884c4645e83c3ed
Sreenu-Paila <sreenu.paila@gmail.com>
2017-10-20 14:40:30 +0200

    Customized index

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,14 +1,18 @@
-package com.devopsbuddy.controller;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author ir17tx
+ *
+ */
 @Controller
-public class HelloWorldController {
+public class IndexController {
 	
 	@RequestMapping("/")
-	private String syaHello() {
+	private String home() {
 		return "index";
 	}
-
 }
